3b603841b7687f22de621511816c2bc3234b3357
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-10-03 15:15:47 +0000

    done with assignment2

---
+++ ICS3U-Assignment2.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2022 Evgeny Vovk All rights reserved.
+#
+# Created by: Evgeny Vovk
+# Created on: October 2022
+# ICS3U-Assignment2.py File, Octagon calculator in python.
+
+import math
+
+
+def main():
+
+    # input
+    side = int(input("Type in the side length of your octagon in centimeters: "))
+
+    # process
+    area = 2 * (1 + math.sqrt(2)) * math.pow(side, 2)
+    perimeter = side * 8
+
+    # output
+    print(
+        "\nThe area of the octagon is {0:,.2f} cm², and the perimeter is {1:,.2f} cm.".format(
+            area, perimeter
+        )
+    )
+
+    print("\n\nDone.")
+
+
+if __name__ == "__main__":
+    main()
